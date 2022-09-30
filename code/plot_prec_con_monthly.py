2c7f6d9bbfa150766c77944229d6aa5c9a8ca7e1
Yan_dellwork <pkmn99@gmail.com>
2022-09-30 17:42:39 +0800

    add T option

--- code/plot_prec_con_monthly.py
+++ code/plot_prec_con_monthly.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 def region_rank(regionlist,df):
     return df.set_index('name').dropna().loc[regionlist].sort_values(by='precYear',ascending=False).index.values
 
-def make_plot(et_data='GLEAM_v3.5a'):
+def make_plot(et_data='GLEAM_v3.5a',var='E'):
     # load data
-    depz=pd.read_csv('../data/processed/prec_con_mon_TP_%s_zonal.csv'%et_data)
+    depz=pd.read_csv('../data/processed/prec_con_mon_TP_%s_%s_zonal.csv'%(et_data,var))
     temp = depz.set_index('name').dropna().sort_values(by='precYear',ascending=False).iloc[:,0:12]
     
     # region list
@@ -39,28 +39,29 @@ def make_plot(et_data='GLEAM_v3.5a'):
     
     sns.heatmap(temp.loc[northwest_order],
                 cmap=cmap, annot=True,
-                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/month)'},
-                vmax=10,xticklabels=range(1,13),ax=axes[0,0]) #,fmt=".2g" two sig figure
+                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/mon)'},
+               # vmax=10,xticklabels=range(1,13),ax=axes[0,0]) #,fmt=".2g" two sig figure
+                vmax=6,xticklabels=range(1,13),ax=axes[0,0],fmt=".2g")
     
     sns.heatmap(temp.loc[northeast_order], cmap=cmap, annot=True,
-                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/month)'},
-                vmax=1,xticklabels=range(1,13),ax=axes[0,1],fmt=".1f")
+                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/mon)'},
+                vmax=1/2,xticklabels=range(1,13),ax=axes[0,1],fmt=".1f")
     
     sns.heatmap(temp.loc[north_order], cmap=cmap, annot=True,
-                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/month)'},
-               vmax=2,xticklabels=range(1,13),ax=axes[1,0],fmt=".1f")
+                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/mon)'},
+               vmax=2/2,xticklabels=range(1,13),ax=axes[1,0],fmt=".1f")
     
     sns.heatmap(temp.loc[east_order], cmap=cmap, annot=True,
-                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/month)'},
-                vmax=2,xticklabels=range(1,13),ax=axes[1,1],fmt=".1f")
+                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/mon)'},
+                vmax=2/2,xticklabels=range(1,13),ax=axes[1,1],fmt=".1f")
     
     sns.heatmap(temp.loc[southwest_order], cmap=cmap, annot=True,
-                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/month)'},
-                vmax=20,xticklabels=range(1,13),ax=axes[2,0])
+                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/mon)'},
+                vmax=20/2,xticklabels=range(1,13),ax=axes[2,0])
     
     sns.heatmap(temp.loc[central_south_order], cmap=cmap, annot=True,
-                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/month)'},
-                vmax=2,xticklabels=range(1,13),ax=axes[2,1],fmt=".1f")
+                linewidths=.5, cbar_kws={"extend":'both',"shrink": .75,"label":'Precipitation contribution (mm/mon)'},
+                vmax=2/2,xticklabels=range(1,13),ax=axes[2,1],fmt=".1f")
     
     # remove ylabel
     [a.set_ylabel('') for a in axes.flatten()]
@@ -69,6 +70,7 @@ def make_plot(et_data='GLEAM_v3.5a'):
     [a.set_title(t,fontsize=12,fontweight='bold') for t,a in zip(['Northwest','Northeast','North','East','Southwest','South'],axes.flatten())]
     
     # disable tick label auto rotation
+    axes[0,0].set_yticklabels(northwest_order,rotation=False)
     axes[1,0].set_yticklabels(north_order,rotation=False)
     axes[0,1].set_yticklabels(northeast_order,rotation=False)
     
@@ -77,8 +79,8 @@ def make_plot(et_data='GLEAM_v3.5a'):
     
     plt.subplots_adjust(hspace=0.3)
     
-    plt.savefig('../figure/figure_prec_con_monthly_0721.png',dpi=300,bbox_inches='tight')
+    plt.savefig('../figure/figure_prec_con_monthly_0915.png',dpi=300,bbox_inches='tight')
     print('figure saved')
 
 if __name__=="__main__":
-    make_plot()
+    make_plot(var='Et')
